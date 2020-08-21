08b905456da4db0cc9e33904a32f3848000bcbe9
Attila <59293104+sweeptic@users.noreply.github.com>
2020-08-21 17:06:09 +0200

    bootstrap card format

--- src/FromServer.js
+++ src/FromServer.js
@@ -2,20 +2,42 @@ import React, { Component } from 'react'
 import Product from './Product'
 import FullProduct from './FullProduct'
 import NewProduct from './NewProduct'
+import Axios from 'axios'
 
 
 export default class FromServer extends Component {
+   state = {
+      products: []
+   }
 
+   componentDidMount() {
+      Axios
+         .get("https://react-http-cec91.firebaseio.com/product.json")
+         .then(response => {
+            this.setState({
+               products: response.data
+            })
+         })
+   }
 
 
    render() {
+      console.log(this.state.products)
+      const products = this.state.products.map((item, index) => {
+         return (
+            <Product
+               productName={item.productName}
+               key={index}
+            />
+         )
+
+      })
+
       return (
          <div className="container">
-            <section className="card-columns">
-               <Product />
-               <Product />
-               <Product />
-            </section>
+            <div className="row">
+               {products}
+            </div>
             <section>
                <FullProduct />
             </section>
--- src/Product.js
+++ src/Product.js
@@ -1,16 +1,34 @@
 import React from 'react'
 
-const Product = () => {
+const Product = (props) => {
    return (
-         <div className="card p-4 mt-3">
-            <div className="card-body">
-               <img className="card-img-top img-fluid" src="https://source.unsplash.com/random/301x200" alt="" />
-               <h4 className="card-title">Card Title</h4>
-               <h6 className="card-subtitle text-muted">Card subtitle</h6>
-               <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, quas.</p>
-               <a className="btn btn-outline-primary" href="#">Read More</a>
+      // <div className="card p-4 mt-3">
+      //    <div className="card-body">
+      //       <img className="card-img-top img-fluid" src="https://source.unsplash.com/random/301x200" alt="" />
+      //       <h4 className="card-title">{props.productName}</h4>
+      //       <h6 className="card-subtitle text-muted">Card subtitle</h6>
+      //       <a className="btn btn-outline-primary" href="#">Read More</a>
+      //    </div>
+      // </div>
+
+
+
+      <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 p-3">
+         <div className="h-100">
+            <div style={{ minHeight: "45vh", maxHeight: "45vh", overflowY: "auto" }} className="card border border-dark bg-white m-3 p-3">
+               <img className="card-img-top img-fluid" src="https://source.unsplash.com/random/301x200" alt={props.index} />
+               <div className="card-body d-flex flex-column">
+                  <h5 className="card-title">{props.productName}</h5>
+                  <a className="mt-auto btn btn-outline-primary" href="#">Read More</a>
+               </div>
+               <div class="card-footer">
+                  <small class="text-muted">Last updated 3 mins ago</small>
+               </div>
             </div>
          </div>
+      </div>
+
+
    )
 }
 
