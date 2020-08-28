3d0cbe24c3a40bf7268b96fe5652b66d6416cf1f
Attila <59293104+sweeptic@users.noreply.github.com>
2020-08-28 12:17:59 +0200

    redux advanced apply logger middleware

--- src/redux_adv/store/reducers/result.jsx
+++ src/redux_adv/store/reducers/result.jsx
@@ -10,7 +10,7 @@ const reducer = (state = initialState, action) => {
       case actionTypes.STORE_RESULT:
          return {
             ...state,
-            results: state.results.concat({ id: new Date(), value: action.result }),
+            results: state.results.concat({ id: Math.random(), value: action.result }),
          }
 
       case actionTypes.DELETE_RESULT:
