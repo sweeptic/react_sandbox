6763c5981f9e1e70b5499f975ef27b8bf73ffa14
Attila <59293104+sweeptic@users.noreply.github.com>
2020-08-29 14:49:55 +0200

    authenticate - logging user out

--- src/redux_auth/Redux_actions_auth.js
+++ src/redux_auth/Redux_actions_auth.js
@@ -22,6 +22,22 @@ export const authFail = (error) => {
    }
 }
 
+export const logout = () => {
+   return {
+      type: actionTypes.AUTH_LOGOUT
+   }
+}
+
+export const checkAuthTimeout = (expirationTime) => {
+   // expirationTime = 5000 
+   console.log('logged out', expirationTime, ' millisec later..')
+   return dispatch => {
+      setTimeout(() => {
+         dispatch(logout());
+      }, expirationTime)
+   }
+}
+
 //hold async code - used by redux-thunk
 export const auth = (email, password, isSignUp) => {
    return dispatch => {
@@ -42,6 +58,7 @@ export const auth = (email, password, isSignUp) => {
          .then(response => {
             console.log(response);
             dispatch(authSuccess(response.data.idToken, response.data.localId));
+            dispatch(checkAuthTimeout(response.data.expiresIn));
          })
          .catch(err => {
             dispatch(authFail(err.response.data.error));
