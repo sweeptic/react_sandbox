8e8892e685389d8089dfa870e0d7b5a911600128
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-10 18:59:35 +0200

    7. Using Multiple State

--- src/FromHooks.js
+++ src/FromHooks.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import { useState } from 'react'
 
-const FromHooks = () => {
+const FromHooks = props => {
+
+   const [todoName, setTodoName] = useState('')
+   const [todoList, setTodoList] = useState([])
+
+
+   const inputChangeHandler = event => {
+      setTodoName(event.target.value)
+   }
+
+   const todoAddHandler = () => {
+      setTodoList(todoList.concat(todoName))
+   }
+
+
    return (
-      <div>
-         hello from hooks
-      </div>
+      <React.Fragment>
+         <h1>Hello from hooks</h1>
+         <input onChange={inputChangeHandler} type="text" placeholder="Todo" value={todoName} />
+         <button onClick={todoAddHandler} type="button">Add</button>
+         <ul>{todoList.map(i => <li key={Math.random()}>{i}</li>)}</ul>
+      </React.Fragment>
    )
 }
 
