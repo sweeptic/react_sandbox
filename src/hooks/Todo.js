0a0aae383ca4f96638211ad14243d72b7648d5f8
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-13 15:50:17 +0200

    23. Creating a Custom Hook

--- src/hooks/Todo.js
+++ src/hooks/Todo.js
@@ -1,30 +1,16 @@
 import React, { useState, useEffect, useReducer, useRef, useMemo } from 'react';
 import axios from 'axios';
 import List from './List';
+import { todoListReducer } from './Reducer'
+import { useFormInput } from './forms';
+
 
 const Todo = props => {
    const [inputIsValid, setInputIsValid] = useState(false);
-   // const [todoName, setTodoName] = useState('');
-   // const [submittedTodo, setSubmittedTodo] = useState(null);
-   // const [todoList, setTodoList] = useState([]);
-   //   const [todoState, setTodoState] = useState({ userInput: '', todoList: [] });
-   const todoInputRef = useRef();
-   const URI = 'https://hooks-44d95.firebaseio.com/todos.json'
-
-   const todoListReducer = (state, action) => {
-      switch (action.type) {
-         case 'ADD':
-            return state.concat(action.payload);
-         case 'SET':
-            return action.payload;
-         case 'REMOVE':
-            return state.filter(todo => todo.id !== action.payload);
-         default:
-            return state;
-      }
-   };
-
    const [todoList, dispatch] = useReducer(todoListReducer, []);
+   // const todoInputRef = useRef();
+   const todoInput = useFormInput();
+   const URI = 'https://hooks-44d95.firebaseio.com/todos.json'
 
    useEffect(() => {
       axios.get(URI).then(result => {
@@ -41,50 +27,16 @@ const Todo = props => {
       };
    }, []);
 
-   const mouseMoveHandler = event => {
-      console.log(event.clientX, event.clientY);
-   };
-
-   const inputValidationHandler = event => {
-      if (event.target.value.trim() === '') {
-         setInputIsValid(false);
-      } else {
-         setInputIsValid(true);
-      }
-   };
-
-   // useEffect(() => {
-   //   document.addEventListener('mousemove', mouseMoveHandler);
-   //   return () => {
-   //     document.removeEventListener('mousemove', mouseMoveHandler);
-   //   };
-   // }, []);
-
-   // useEffect(
-   //   () => {
-   //     if (submittedTodo) {
-   //       dispatch({ type: 'ADD', payload: submittedTodo });
-   //     }
-   //   },
-   //   [submittedTodo]
-   // );
-
-   // const inputChangeHandler = event => {
-   //   // setTodoState({
-   //   //   userInput: event.target.value,
-   //   //   todoList: todoState.todoList
-   //   // });
-   //   setTodoName(event.target.value);
+   // const inputValidationHandler = event => {
+   //    if (event.target.value.trim() === '') {
+   //       setInputIsValid(false);
+   //    } else {
+   //       setInputIsValid(true);
+   //    }
    // };
 
    const todoAddHandler = () => {
-      // setTodoState({
-      //   userInput: todoState.userInput,
-      //   todoList: todoState.todoList.concat(todoState.userInput)
-      // });
-
-      const todoName = todoInputRef.current.value;
-
+      const todoName = todoInput.value;
       axios
          .post(URI, { name: todoName })
          .then(res => {
@@ -112,9 +64,11 @@ const Todo = props => {
          <input
             type="text"
             placeholder="Todo"
-            ref={todoInputRef}
-            onChange={inputValidationHandler}
-            style={{ backgroundColor: inputIsValid ? 'transparent' : 'red' }}
+            onChange={todoInput.onChange}
+            value={todoInput.value}
+            // ref={todoInputRef}
+            // onChange={inputValidationHandler}
+            style={{ backgroundColor: todoInput.validity === true ? 'transparent' : 'red' }}
          />
          <button type="button" onClick={todoAddHandler}>
             Add
