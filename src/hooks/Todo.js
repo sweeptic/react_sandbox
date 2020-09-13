237953b4c09ecac32e40cf356f27fe77009820f1
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-13 14:57:12 +0200

    clean up

--- src/hooks/Todo.js
+++ src/hooks/Todo.js
@@ -1,54 +1,33 @@
 import React from 'react'
-import { useState, useEffect, useReducer, useRef } from 'react'
+import { useEffect, useReducer, useRef } from 'react'
 import Axios from 'axios'
 import { todoListReducer } from './reducer'
 
 const Todo = () => {
 
-   // const [todoName, setTodoName] = useState('');
    const [todoList, dispatch] = useReducer(todoListReducer, []);
    const todoInputRef = useRef();
+   const URI = 'https://hooks-44d95.firebaseio.com/todos.json'
 
-
-   //component run for the first time and every render cycle
-   //2 arguments
    useEffect(() => {
       console.log('useEffect run')
-      Axios.get('https://hooks-44d95.firebaseio.com/todos.json').then(result => {
+      Axios.get(URI).then(result => {
          console.log(result);
          const todoData = result.data;
          const todos = []
          for (const key in todoData) {
             todos.push({ id: key, name: todoData[key].name })
          }
-         // setTodoList(todos);
          dispatch({ type: 'SET', payload: todos });
       });
       return () => {
          console.log('cleanup')
       }
-   }, []) //when this change ..
-   // replicate component did mount -> pass empty array
-   // replicate component did mount + did update pass [todoName]
-
-
-   // useEffect(() => {
-   //    if (submittedTodo) {
-   //       dispatch({ type: 'ADD', payload: submittedTodo })
-   //    }
-   // }, [submittedTodo]);
-
-   // const inputChangeHandler = event => {
-   //    // console.log(todoList) 
-   //    setTodoName(event.target.value)
-   // }
-
+   }, [])
 
    const todoAddHandler = () => {
-
       const todoName = todoInputRef.current.value;
-
-      Axios.post('https://hooks-44d95.firebaseio.com/todos.json', { name: todoName })
+      Axios.post(URI, { name: todoName })
          .then(res => {
             setTimeout(() => {
                const todoItem = { id: res.data.name, name: todoName };
