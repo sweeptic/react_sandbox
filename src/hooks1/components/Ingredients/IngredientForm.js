104b04019baf34c743419ed619e3a07222b8f762
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-16 18:21:22 +0200

    getting started with usestate

--- src/hooks1/components/Ingredients/IngredientForm.js
+++ src/hooks1/components/Ingredients/IngredientForm.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 import Card from '../UI/Card';
 import style from './IngredientForm.module.css';
+import { useState } from 'react';
 
 const IngredientForm = React.memo(props => {
+
+
+  //usestate return this two elements
+  //state: current state snapshot. always return initial or updated state
+  //react manages detach this, therefore this state will survive re renders. 
+
+  //second element: functions what use for update the state. dispatch ----
+  const inputState = useState({ title: '', amount: '' })
+
   const submitHandler = event => {
     event.preventDefault();
-    // ...
   };
 
   return (
@@ -15,11 +24,21 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className={style.formControl}>
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input
+              type="text"
+              id="title"
+              value={inputState[0].title}
+              onChange={event => inputState[1]({ title: event.target.value })}
+            />
           </div>
           <div className={style.formControl}>
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input
+              type="number"
+              id="amount"
+              value={inputState[0].amount}
+              onChange={event => inputState[1]({ amount: event.target.value })}
+            />
           </div>
           <div className={style.ingredientForm__actions}>
             <button type="submit">Add Ingredient</button>
