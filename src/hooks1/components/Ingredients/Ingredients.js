053d3fd66f9ad02594b1326a9ccc6c8c7594790f
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-18 19:16:00 +0200

    multiple states

--- src/hooks1/components/Ingredients/Ingredients.js
+++ src/hooks1/components/Ingredients/Ingredients.js
@@ -2,14 +2,28 @@ import React from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
+import { useState } from 'react';
+import IngredientList from './IngredientList';
 
-function Ingredients() {
+const Ingredients = () => {
+
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    setUserIngredients(prevIngredients =>
+      [...prevIngredients, { id: Math.random().toString(), ...ingredient }]
+    )
+  }
+
+
   return (
     <div className="App">
-      <IngredientForm />
+
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
+        < IngredientList ingredients={userIngredients} onRemoveItem={() => { }} />
         {/* Need to add list here! */}
       </section>
     </div>
